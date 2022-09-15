9c9ff93968a0d2fda7f2f8c326282ea5a35a8903
Pedro <pcampos952@gmail.com>
2022-09-15 20:50:28 -0300

    Inversão de matriz (não funcional)

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import pandas as pd
-
+import numpy as np
 df = pd.read_excel("FECHAMENTO_MAIS__NEGOCIADAS_5minutos.xls")
+x = np.asarray(df)
+line1 = 0
+line2 = 12
+while line2 < 6200 :
+    print(x[line1:line2])
+    arr_inv = x[line1:line2]
+    arr_inv = np.linalg.inv(arr_inv)
+    print("\n\n")
+    print(arr_inv)
+    print("\n\n\n")
+    line1=line1+1
+    line2=line2+1
 
-x = df.values.tolist()
-
-matrix = [x]
-while x != []:
-    matrix.append(x[:2])
-    x = x[2:]
-
-print (matrix)
 
