8b76ab0827b3de0bd823c00614cdbb2c4e883781
Pedro <pcampos952@gmail.com>
2022-09-23 19:24:08 -0300

    Tratamento de erro de matriz singular

--- PythonProgram.py
+++ PythonProgram.py
@@ -1,20 +1,25 @@
+from operator import length_hint
 import pandas as pd
 import numpy as np
 from datetime import datetime
 df = pd.read_excel("FECHAMENTO_MAIS__NEGOCIADAS_5minutos.xls")
 x = np.asarray(df)
 matrix = np.delete(x, [0], 1)
-matrix_float = matrix.astype(np.float64)
-print (matrix)
+matrix_float = matrix.astype(np.float32)
 line1 = 0
 line2 = 12
-while line2 < 6200:
+while line2 <= len(matrix_float):
     print(matrix_float[line1:line2])
-    arr_inv = matrix_float[line1:line2]
-    arr_inv = np.linalg.inv(arr_inv)
-    print("\n")
-    print(arr_inv)
-    print("\n\n\n")
+    det = np.linalg.det(matrix_float[line1:line2])
+    det = round(det,15)
+    if (det != 0):
+        arr_inv = matrix_float[line1:line2]
+        arr_inv = np.linalg.inv(arr_inv)
+        print("\n")
+        print(arr_inv)
+        print("\n\n")
+    elif (det == 0):
+        print("\nNao foi possivel realizar a matriz inversa por determinante igual a 0\n\n")
     line1 = line1+1
     line2 = line2+1
 
